dc995bbf24161e5114009ac14529c96c9676061e
Alireza <alireza.7m@gmail.com>
2025-11-27 18:50:32 +0330

    Update MainWindow and CustomerRegistrationWidget

--- GUI/OrderRegistrationWindow.py
+++ GUI/OrderRegistrationWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QMessageBox
 from GUI.CustomerRegistrationWidget import CustomerRegistrationWidget
 from GUI.ProductsOrderingWidget import ProductsOrderingWidget
 
@@ -6,30 +6,42 @@ from GUI.ProductsOrderingWidget import ProductsOrderingWidget
 class OrderRegistrationWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.layout = None
-        self.customer_Registration_widget = None
+        self.customer_registration_widget = None
         self.product_ordering_widget = None
         self.initUI()
 
     def initUI(self):
-        self.layout = QVBoxLayout()
-        self.customer_Registration_widget = CustomerRegistrationWidget()
-        self.customer_Registration_widget.customer_confirmation_button.clicked.connect(
+        layout = QVBoxLayout()
+        self.customer_registration_widget = CustomerRegistrationWidget()
+        self.customer_registration_widget.customer_confirmation_button.clicked.connect(
             self.retrieve_order)
-        self.layout.addWidget(self.customer_Registration_widget)
-        self.setLayout(self.layout)
+        layout.addWidget(self.customer_registration_widget)
+        self.setLayout(layout)
         self.setWindowTitle('Customer Registration Window')
-        self.setGeometry(100, 60, 400, 180)
+        self.setGeometry(100, 60, 600, 240)
 
-    def show_product_ordering_widget(self):
-        self.product_ordering_widget.setDisabled(False)
-        self.product_ordering_widget.show()
+    # def show_product_ordering_widget(self):
+    #     self.product_ordering_widget.setDisabled(False)
+    #     self.product_ordering_widget.show()
 
     def retrieve_order(self):
-        order_repo = self.customer_Registration_widget.get_order_repository()
+        order_repo = self.customer_registration_widget.get_order_repository()
         self.product_ordering_widget = ProductsOrderingWidget(order_repo)
         #order_repo = self.customer_Registration_widget.get_order_repository()
         #self.product_ordering_widget.set_order_repository(order_repo)
-        self.layout.addWidget(self.product_ordering_widget)
+        self.layout().addWidget(self.product_ordering_widget)
         self.setGeometry(100, 45, 750, 600)
         self.product_ordering_widget.show()
+
+    def closeEvent(self, event):
+        msg = QMessageBox(self)
+        msg.setIcon(QMessageBox.Warning)
+        msg.setWindowTitle("Exit Confirmation")
+        msg.setText("Are you sure you want to close the window?")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        msg.setDefaultButton(QMessageBox.No)
+
+        if msg.exec_() == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
